3feb2246e01f5630c4bfd67f150d408638d44f15
Foued Amiri <fouedamiri10@gmail.com>
2024-08-08 08:58:11 +0100

    Creat ThemeProvider.jsx file for dark mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,32 +1,34 @@
-import Navbar from '@/components/navbar/Navbar';
-import './globals.css'
-import { Inter } from 'next/font/google'
-import Footer from '@/components/footer/Footer';
-import AuthProvider from '@/providers/AuthProvider';
-import { ThemeContextProvider } from '@/context/ThemeContext';
+import Navbar from "@/components/navbar/Navbar";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Footer from "@/components/footer/Footer";
+import AuthProvider from "@/providers/AuthProvider";
+import { ThemeContextProvider } from "@/context/ThemeContext";
+import ThemeProvider from "@/providers/ThemeProvider";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Blog App',
-  description: 'The best blog app!',
-}
+  title: "Blog App",
+  description: "The best blog app!",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
         <ThemeContextProvider>
-        
-        <AuthProvider>
-          <div className="container">
-            <div className="wrapper">
-              <Navbar />
-              {children}
-              <Footer />
-            </div>
-          </div>
-        </AuthProvider>
+          <ThemeProvider>
+            <AuthProvider>
+              <div className="container">
+                <div className="wrapper">
+                  <Navbar />
+                  {children}
+                  <Footer />
+                </div>
+              </div>
+            </AuthProvider>
+          </ThemeProvider>
         </ThemeContextProvider>
       </body>
     </html>
